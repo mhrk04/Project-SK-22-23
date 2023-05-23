4081a59b35426ad7b4aa03e8bfd037df1c46fc5c
mhrk04 <haziqrohaizan@gmail.com>
2023-05-23 21:07:34 +0800

    update sql

---
+++ Database/README.md
@@ -0,0 +1,3 @@
+# Database
+
+> mengandungi file sql untuk membuat database
--- Database/pilihan.sql
+++ Database/pilihan.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE pilihan;
+
 CREATE TABLE jenama (
   idJenama int(11) AUTO_INCREMENT PRIMARY KEY,
   namaJenama varchar(50)
