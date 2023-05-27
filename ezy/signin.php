2cecd000278ee1ae8fa5ed67d99402ebb1dd2794
mhrk04 <haziqrohaizan@gmail.com>
2023-05-27 20:29:47 +0800

    add signin.php

---
+++ ezy/signin.php
@@ -0,0 +1,14 @@
+<h3>LOG MASUK</h3>
+<form method="post" action="signin_semak.php">
+  <div>
+    ID Anda: <br>
+    <input type="text" name="id" placeholder="TAIP DI SINI" maxlength="10" size="15%" required autofocus>
+    <br>
+    Katalaluan: <br>
+    <input type="password" name="pass" placeholder="******" size="15%" required>
+  </div>
+  <div>
+    <button name="hantar" type="submit">LOGIN</button>
+    <button type="reset">RESET</button>
+  </div>
+</form>
